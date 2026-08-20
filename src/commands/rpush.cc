4b990683afb2810ae4955b968b37e53788638510
Vadim Mitko <vadim@vadimmitko.com>
2026-08-20 15:29:54 +0300

    Add multiple items to list with RPUSH

--- src/commands/rpush.cc
+++ src/commands/rpush.cc
@@ -5,22 +5,27 @@
 #include <vector>
 
 RespValue cmd_rpush(std::vector<std::string> &&args, RedisDb &db) {
-  if (args.size() != 2) return RespError { "wrong number of arguments" };
+  if (args.size() < 2) return RespError { "wrong number of arguments" };
 
-  RedisObject* l = db.get_mutable(args[0]);
+  RedisObject* l_ptr = db.get_mutable(args[0]);
+  size_t n = args.size();
 
-  if (l == nullptr) {
+  if (l_ptr == nullptr) {
     std::vector<ListObject> l;
-    l.push_back(std::move(args[1]));
-    
-    db.set_value(std::move(args[0]), std::move(l));
+    l.reserve(n-1);
 
-    return Integer { 1 };
+    for (size_t i = 1; i < n; i++) l.emplace_back(std::move(args[i]));
+
+    db.set_value(std::move(args[0]), std::move(l));
+    
+    return Integer { static_cast<int64_t>(n-1) };
   }
 
-  auto* list = std::get_if<std::vector<ListObject>>(l);
-  if (!list)
-      return RespError{"WRONGTYPE Operation against a key holding the wrong kind of value"};
-  list->push_back(std::move(args[1]));
-  return Integer{static_cast<int64_t>(list->size())};
+  auto* list = std::get_if<std::vector<ListObject>>(l_ptr);
+  if (!list) return RespError{ "WRONGTYPE Operation against a key holding the wrong kind of value" };
+
+  list->reserve(list->size() + n-1);
+  for (size_t i = 1; i < n; i++) list->emplace_back(std::move(args[i]));
+
+  return Integer{ static_cast<int64_t>(list->size()) };
 }
